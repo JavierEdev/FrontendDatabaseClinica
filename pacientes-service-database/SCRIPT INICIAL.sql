665ce15d1e76eec45ac524e759e54be61ca06686
JAVIER_PC\javie <Javier Estrada>
2025-09-02 22:49:04 -0600

    Fixing branch

--- pacientes-service-database/SCRIPT INICIAL.sql
+++ pacientes-service-database/SCRIPT INICIAL.sql
@@ -1,13 +1,4 @@
 -- ===========================
--- Crear Base de Datos
--- ===========================
-CREATE DATABASE IF NOT EXISTS pacientes_service 
-  CHARACTER SET utf8mb4 
-  COLLATE utf8mb4_unicode_ci;
-
-USE pacientes_service;
-
--- ===========================
 -- Tabla: Pacientes
 -- ===========================
 CREATE TABLE IF NOT EXISTS Pacientes (
